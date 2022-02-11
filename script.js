aa8cc42f71e091ea40fa8865f93c13e7f06b08b9
Himanshu <himanshumehta8696@gmail.com>
2022-02-11 21:51:50 +0530

    My percentage calcy

---
+++ script.js
@@ -0,0 +1,32 @@
+const calcy = () => {
+    let maths = document.getElementById('maths').value;
+    let comp = document.getElementById('computer').value;
+    let dataSa = document.getElementById('dsa').value;
+    let wd = document.getElementById('wd').value;
+    let cplus = document.getElementById('cplusplus').value;
+    let grades = "";
+    // console.log(maths);
+    let totalGrad = parseFloat(maths) + parseFloat(comp) + parseFloat(dataSa) + parseFloat(wd) + parseFloat(cplus);
+
+    let perc = (totalGrad / 500) * 100;
+
+    if (perc <= 100 && perc >= 80) {
+        grades = 'A';
+    }
+    else if (perc <= 79 && perc >= 60) {
+        grades = 'B'
+    }
+    else if (perc <= 59 && perc >= 40) {
+        grades = 'C';
+    }
+    else {
+        grades = 'F';
+    }
+    if (perc >= 39) {
+
+        document.getElementById('showData').innerHTML = `Out of 500 your total is ${totalGrad} and percentage is ${perc}%. <br> Your grade is ${grades} your are Pass`;
+    }
+    else {
+        document.getElementById('showData').innerHTML = `Out of 500 your total is ${totalGrad} and percentage is ${perc}%. <br> Your grade is ${grades} your are Fail`;
+    }
+}
